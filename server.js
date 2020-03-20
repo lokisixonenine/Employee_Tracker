372a0d022e8cdd96d84d0c6705a9ab38ec7d0fa4
Jaycee Bagtas <jaycee.bagtas@gmail.com>
2020-03-20 14:57:33 -0700

    installed dependencies and added connections to server.js

--- server.js
+++ server.js
@@ -15,3 +15,24 @@ const interFace = [
     "Remove Employee's Role",
     "Exit Employee Tracker"
 ];
+
+let listDepartment;
+let listEmployeeRoles;
+let listEmployees;
+
+connection.query("SELECT * FROM departments", function (err, res) {
+    if (err) throw err;
+    listDepartment = res.map(role => ({ name: dep.title, value: dep.id }));
+});
+
+connection.query("SELECT * FROM roles", function(err, res) {
+    if (err) throw err;
+    listEmployeeRoles = res.map(role => ({ name: role.title, value: role.id}));
+});
+
+connection.query("SELECT * FROM employees", function (err, res) {
+    if (err) throw err;
+    listEmployees = res.map(role => ({ name: `${emp.first_name}${emp.last_name}`,
+    value: emp.id
+    }));
+});
